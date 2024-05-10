666639c08b1d3558e40f43b7d1d3df18ed9d903f
weisuofayubd <weisuofayubd@outlook.com>
2024-05-10 09:47:06 +0800

    Update MainViewModel.java
    Signed-off-by: weisuofayubd <weisuofayubd@outlook.com>

--- app/src/main/java/io/github/muntashirakon/AppManager/main/MainViewModel.java
+++ app/src/main/java/io/github/muntashirakon/AppManager/main/MainViewModel.java
@@ -450,7 +450,6 @@ public class MainViewModel extends AndroidViewModel implements ListOptions.ListO
                     }
                     // Filter rests
                     if ((mFilterFlags & MainListOptions.FILTER_FROZEN_APPS) != 0 && !item.isDisabled) {
-                        Log.e("GGGGGGGG", "package: " + item.packageName);
                         continue;
                     } else if ((mFilterFlags & MainListOptions.FILTER_APPS_WITH_RULES) != 0 && item.blockedCount <= 0) {
                         continue;
